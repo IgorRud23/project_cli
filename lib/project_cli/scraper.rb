c39119c44a87faa5451b3e70a8827257d39353d2
Igor Rud <rud.igor23@gmail.com>
2019-04-07 23:57:28 +0000

    changed link for iteration

--- lib/project_cli/scraper.rb
+++ lib/project_cli/scraper.rb
@@ -2,8 +2,8 @@ class Scraper
   def scrape_page
     html = open("https://www.lifewire.com/cool-websites-to-look-at-when-bored-3486362")
   doc = Nokogiri::HTML(html)
-  doc.css("div.comp.article-content").each do |site|
-  binding.pry
+  doc.css("div#list-sc-item_1-0.comp.mntl-sc-list-item.list-sc-item.mntl-block").each do |site|
+
 end 
 end
 end 

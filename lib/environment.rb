91f22b05487acd4054782f8e575d43a2c06c8116
Igor Rud <rud.igor23@gmail.com>
2019-04-07 18:53:54 +0000

    change enviroment

---
+++ lib/environment.rb
@@ -0,0 +1,12 @@
+#require "project_cli/version"
+require_relative './project_cli/version'
+require_relative './project_cli/cli'
+
+require 'pry'
+require 'nokogiri'
+
+
+module ProjectCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
--- lib/project_cli.rb
+++
@@ -1,6 +0,0 @@
-require "project_cli/version"
-
-module ProjectCli
-  class Error < StandardError; end
-  # Your code goes here...
-end

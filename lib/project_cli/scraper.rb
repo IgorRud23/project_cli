5b2ec7039b5a3f24c489a577c34ae799ed0c8c6b
Igor Rud <rud.igor23@gmail.com>
2019-04-07 21:37:29 +0000

    change word spel

--- lib/project_cli/scraper.rb
+++ lib/project_cli/scraper.rb
@@ -1,7 +1,7 @@
-class Scarper
+class Scraper 
   def scrape_page
     html = open("https://www.lifewire.com/cool-websites-to-look-at-when-bored-3486362")
   doc = Nokogiri::HTML(html)
-  #binding.pry
+  binding.pry
 end
 end 

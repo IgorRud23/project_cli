f0c98200a3c0941df36e507742156ccce81da0d2
Igor Rud <rud.igor23@gmail.com>
2019-04-07 21:46:40 +0000

    did testing iteration on scraper

--- lib/project_cli/scraper.rb
+++ lib/project_cli/scraper.rb
@@ -2,6 +2,7 @@ class Scraper
   def scrape_page
     html = open("https://www.lifewire.com/cool-websites-to-look-at-when-bored-3486362")
   doc = Nokogiri::HTML(html)
+  doc.css("div.comp.article-content").each do |site|
   binding.pry
 end
 end 

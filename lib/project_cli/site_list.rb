da04bef5e561334dc3a7029c6be76f22c03a710c
Igor Rud <rud.igor23@gmail.com>
2019-04-08 21:46:12 +0000

    add changes to SiteList class

--- lib/project_cli/site_list.rb
+++ lib/project_cli/site_list.rb
@@ -1,5 +1,13 @@
 class SiteList 
-  attr_accessor :heading
+  attr_accessor :heading, :info_one, :info_two
   
+  @@all = []
+  
+  def initialize(heading)
+  @heading = heading
+  @info_one = info_one
+  @info_two = info_two
+  @@all << self 
+end 
   
 end 

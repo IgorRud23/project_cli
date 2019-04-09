38f6868a83b8f80f7a6cd72b1d318dbde80b6b96
Igor Rud <rud.igor23@gmail.com>
2019-04-09 15:25:24 +0000

    test vers

--- lib/project_cli/scraper.rb
+++ lib/project_cli/scraper.rb
@@ -3,12 +3,26 @@ class Scraper
   def scrape_page
     html = open("https://www.lifewire.com/cool-websites-to-look-at-when-bored-3486362")
   doc = Nokogiri::HTML(html)
-  doc.css("div#list-sc_1-0.comp.article-content.list.no-commerce.list-sc.mntl-block.chomp-content").each do |site|
-binding.pry 
-heading =  site.css("a.mntl-sc-block-heading__link").text  
+  
+  
+  listOfTitles
+  list = doc.at("#list-sc_1-0")
+  list.search("div").each do |div|
+    listOfTitles = div.search('h3').map { |name| name.text }
+   
+   
+  # doc.css("#list-sc_1-0").css("div").css("h3")each do |el|
+    
+  # doc.css("#list-sc_1-0 div h3")each do |el|
+    
+puts el.text
+
+
+  binding.pry
 # info_one = site.css("div#mntl-sc-block_2-0-2").text.strip
 # info_two = site.css("div#mntl-sc-block_2-0-3").text.strip
 SiteList.new(heading)
+end 
 end
 end
 

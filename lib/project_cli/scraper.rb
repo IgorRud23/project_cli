4a856681b42424ed3c7b1e473ead7ebbae8910e0
Igor Rud <rud.igor23@gmail.com>
2019-04-10 00:07:12 +0000

    change scraper

--- lib/project_cli/scraper.rb
+++ lib/project_cli/scraper.rb
@@ -7,18 +7,7 @@ class Scraper
     html = open("https://www.lifewire.com/cool-websites-to-look-at-when-bored-3486362")
   doc = Nokogiri::HTML(html)
   
-  
-  # listOfTitles
-  # list = doc.at("#list-sc_1-0")
-  # list.search("div").each do |div|
-  #   listOfTitles = div.search('h3').map { |name| name.text }
-  # doc.css("#list-sc_1-0").css("div").css("h3")each do |el|
-  # doc.css("#list-sc_1-0 div h3")each do |el|
-  
   headingList = doc.css("h3").children.map { |name| name.text}.select { |title|  title.length > 1 }
-  
-  #headingList = Hash[(0...headingArray.size).zip headingArray]
-
  
 # info_one = site.css("div#mntl-sc-block_2-0-2").text.strip
 # info_two = site.css("div#mntl-sc-block_2-0-3").text.strip

a283d12886be1d86a164d4ec7ca76c6e359f18de
Igor Rud <rud.igor23@gmail.com>
2019-04-10 00:27:18 +0000

    changed cli, did prompt

--- lib/project_cli/cli.rb
+++ lib/project_cli/cli.rb
@@ -10,16 +10,27 @@ class CLI
   def menu
    navigation = SiteList.all
    navigation.each do |site_name|
+     
       site_name.headingList.each_with_index do |name, index|
         puts "#{index+1}. #{name}" 
-      
 end 
 end 
   end 
     
     def prompt
-      selected = gets.chomp
+    
+      puts "Select topic by number"
+      # vaidation of select
+      
+      selected = gets.chomp.to_i 
+      if (selected > 0 )
+        puts "is number"
+      else 
+        puts "not a number"
+      
+       
       puts selected 
+    end 
     end 
     
 end 
